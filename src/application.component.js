b1d007b54994657e24f0452ee9e585502d001058
meiriko <meir.shahar@gmail.com>
2017-01-15 23:49:47 +0200

    removed ui-view place holders

--- src/application.component.js
+++ src/application.component.js
@@ -5,7 +5,7 @@ const appComponent = {
   template: `
         <div class="container-fluid">
           <ui-view class="list"></ui-view>
-          <ui-view class="content" name="content"><div>xxx</div><div>yyy</div></ui-view>
+          <ui-view class="content" name="content"></ui-view>
         </div>
     `,
   controller: AppController
